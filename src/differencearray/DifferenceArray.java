7d0ea0a534820f15194f505db846bf7f0762b06f
Wilson Castiblanco <wcastiblancoq@gmail.com>
2016-11-21 22:54:38 -0500

    Solution using has maps

--- src/differencearray/DifferenceArray.java
+++ src/differencearray/DifferenceArray.java
@@ -5,7 +5,7 @@
  */
 package differencearray;
 
-import java.util.Hashtable;
+import java.util.HashMap;
 
 /**
  *
@@ -17,7 +17,9 @@ public class DifferenceArray {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        getPairsOfIntegersByDifference();
+        int difference = 3;
+        int[] integersArray = new int[]{3, 8, 15, 1, 9, 6, 4, 12, 20};
+        getPairsOfIntegersByDifference(integersArray, difference);
     }
 
     /*
@@ -25,27 +27,20 @@ Given an array of distinct integer values, count the number of pairs of integers
 
 e.g: (7, 9) (1, 3), (5, 3)
      */
-    public static void getPairsOfIntegersByDifference() {
-        int difference = 2;
-        int[] integersArray = new int[]{1, 7, 5, 9, 2, 12, 3};
-
-        String pairs = "";
-        
-        Hashtable<Integer, Integer> hashIntegers = new Hashtable<>(integersArray.length);
-        
-       for (int i = 0; i < integersArray.length; i++) {
-           
-       }
-
-        for (int i = 0; i < integersArray.length; i++) {
-            for (int j = 0; j < integersArray.length; j++) {
-                if (integersArray[i] - integersArray[j] == difference || integersArray[j] - integersArray[i] == difference ) {
-                    pairs += " (" + integersArray[i] + "," + integersArray[j] + ") ";
-                }
-            }
+    public static void getPairsOfIntegersByDifference(int a[], int subValue) {
+        HashMap store = new HashMap();
+        for (int i = 0; i < a.length; i++) {
+            store.put(a[i], 1);
         }
-
-        System.out.println(pairs);
+        for (int i = 0; i < a.length; i++) {
+            if (store.containsKey(a[i] - subValue)) {
+                System.out.println("(" + a[i] + "," + (a[i] - subValue) + ")");
+            }
+            if (store.containsKey(a[i] + subValue)) {
+                System.out.println("(" + a[i] + "," + (a[i] + subValue) + ")");
+            }
+            store.remove(a[i]);
+        }
     }
 
 }
